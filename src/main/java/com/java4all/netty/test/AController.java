66d9cf659b7b2837e917490abfe2c0acdfd8f71c
ITyunqing <1186355422@qq.com>
2019-10-11 20:13:28 +0800

    add

--- src/main/java/com/java4all/netty/test/AController.java
+++ src/main/java/com/java4all/netty/test/AController.java
@@ -1,5 +1,6 @@
 package com.java4all.netty.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("aController")
 public class AController {
 
+    @Autowired
+    private AService aService;
+
     ThreadLocal threadLocal = new ThreadLocal();
 
     @GetMapping("a")
     public String a(){
         threadLocal.set(1111111);
         System.out.println("AController#a 线程名称="+Thread.currentThread().getName()+"threadLocal value="+threadLocal.get());
+
+        aService.a();
 
         new Thread(new Runnable() {
             @Override
--- src/main/java/com/java4all/netty/test/AServiceImpl.java
+++ src/main/java/com/java4all/netty/test/AServiceImpl.java
@@ -1,13 +1,17 @@
 package com.java4all.netty.test;
 
+import org.springframework.stereotype.Service;
+
 /**
  * @author wangzhongxiang
  * @date 2019年10月11日 20:04:23
  */
+@Service
 public class AServiceImpl implements AService{
+    ThreadLocal threadLocal = new ThreadLocal();
 
     @Override
     public void a() {
-
+        System.out.println("AServiceImpl#a 线程名称="+Thread.currentThread().getName()+"threadLocal value="+threadLocal.get());
     }
 }
