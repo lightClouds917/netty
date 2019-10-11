793fee45c26be694184b7ae39ec36b08cf001ee0
ITyunqing <1186355422@qq.com>
2019-10-11 20:09:13 +0800

    add test

--- src/main/java/com/java4all/netty/test/AController.java
+++ src/main/java/com/java4all/netty/test/AController.java
@@ -12,9 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("aController")
 public class AController {
 
+    ThreadLocal threadLocal = new ThreadLocal();
+
     @GetMapping("a")
     public String a(){
+        threadLocal.set(1111111);
+        System.out.println("AController#a 线程名称="+Thread.currentThread().getName()+"threadLocal value="+threadLocal.get());
 
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("AController#a 中其他线程 线程名称="+Thread.currentThread().getName()+"threadLocal value="+threadLocal.get());
+            }
+        }).start();
         return "aaa";
     }
 }
