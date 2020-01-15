b2668954a87efefb58a33301d32f0c40fae70298
ITyunqing <1186355422@qq.com>
2020-01-15 21:11:06 +0800

    mod:add

--- src/main/java/com/java4all/netty/base/server/EchoServerHandler2.java
+++ src/main/java/com/java4all/netty/base/server/EchoServerHandler2.java
@@ -10,36 +10,43 @@ public class EchoServerHandler2 extends ChannelInboundHandlerAdapter{
 
     @Override
     public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("EchoServerHandler2 has channelRegistered");
         super.channelRegistered(ctx);
     }
 
     @Override
     public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("EchoServerHandler2 has channelUnregistered");
         super.channelUnregistered(ctx);
     }
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("EchoServerHandler2 has channelActive");
         super.channelActive(ctx);
     }
 
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("EchoServerHandler2 has channelInactive");
         super.channelInactive(ctx);
     }
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("EchoServerHandler2 has channelRead");
         super.channelRead(ctx, msg);
     }
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("EchoServerHandler2 has channelReadComplete");
         super.channelReadComplete(ctx);
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println("EchoServerHandler2 has exceptionCaught");
         super.exceptionCaught(ctx, cause);
     }
 }
